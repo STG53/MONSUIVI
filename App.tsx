602dd3b69586b89bc85de57d5dbf552774b9820d
stg53 <sgoubaud.sg@gmail.com>
2025-03-07 18:54:40 +0100

    essai de commit 2

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import dashboardStyles from "./assets/styles/style";
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={dashboardStyles.titre1SMP}>Mon suivi</Text>
+      <Text style={dashboardStyles.titre1SMP}>Mon suivi modif</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
